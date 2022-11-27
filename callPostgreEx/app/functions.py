0e190e19451c025abd178b8beaca1d9f5122a899
bujarkreci <71174797+bujarkreci@users.noreply.github.com>
2022-11-27 03:09:21 +0100

    20221127-1

--- callPostgreEx/app/functions.py
+++ callPostgreEx/app/functions.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
 import requests
+from bs4 import BeautifulSoup
 
 def extractDataset(docpath):
     try:
@@ -15,12 +16,100 @@ def extractDataset(docpath):
     except:
         return print("Error occured with the dataset.")
 
-class Teams:
+class Teams:    
+    
     def __init__(self, teamid, teamname, link):
         self.teamid = teamid
         self.teamname = teamname
-        self.link = link                
+        self.link = link          
+        
+    def __str__(self):
+        return dict(self)
 
+    def __repr__(self):
+        return str(self.__str__())
+    
+    def __iter__(self):
+        yield from {
+            "team id:": self.teamid,
+            "team name": self.teamname,
+            "team head": self.getTeamsHeadSoup()
+        }.items()  
+
+        
+    def getTeamsHeadSoup(self):
+        """ Get teams """
+
+        try:
+            data = []
+            # fill dataset            
+            page = requests.get(self.link)            
+            soup = BeautifulSoup(page.text, parser='lxml', features="lxml")            
+            table = soup.find("table", {"class":"infobox"} )
+            table_body = table.find('tbody')
+            rows = table_body.find_all('tr')            
+            for row in rows:
+                cols = row.find_all(['th','td'])    
+                cols = [ele.text.strip() for ele in cols if ele]    
+                data.append(cols[0:2]) # Get rid of empty values    
+            tab1 = pd.DataFrame(data)
+            cols = {0:"ind",1:"val"}
+            columns = list(cols.values())
+            tab1 = tab1.rename(columns=cols)
+            tab1 = tab1.loc[:,columns]
+            tab1 = tab1.dropna()
+            datas = ["Head coach","FIFA code","Confederation","Captain"]
+            dat = tab1.loc[(tab1["ind"].isin(datas))]  
+            res = dat.set_index("ind").T
+            res["teamid"] = self.teamid
+            res = res.to_dict(orient="records")
+            return res
+        except:
+            return print("Error occured with the link.")
+    
+    def getTeamsDetailsSoup(self):
+        try:
+            data = []   
+            page = requests.get(self.link)
+            soup = BeautifulSoup(page.text, parser='lxml', features="lxml")            
+            table = soup.find( "table", {"class":"plainrowheaders"} )
+            if table.empty:
+                raise Exception("There is an empty dataset")
+                
+            table_body = table.find('tbody')
+            
+            rows = table_body.find_all('tr')
+            for row in rows:
+                cols = row.find_all(['td','th'])    
+                states = row.find_all('img')
+                cols = [ele.text.strip() for ele in cols if ele]  
+                cols1 = [ele.get('alt') for ele in states] 
+                datas1 = [ele for ele in cols]
+                datas2 = [ele for ele in cols1]
+                res = [*datas1, *datas2]
+                data.append(res) # Get rid of empty values    
+              
+            ColumnNames = {"No.":"Number","Pos.":"Position","Player":"Player name","Date of birth (age)":"Date of birth",
+                   "Club":"Current club",None:"Country"}
+            tab1 = pd.DataFrame(data)                        
+            tab1.columns = tab1.iloc[0]            
+            tab1 = tab1[1:]
+            tab1 = tab1.dropna()
+            tab1 = tab1.rename(columns=ColumnNames)
+            tab1["Position"] = tab1["Position"].str[1:]
+            tab1["Date of birth"] = tab1["Date of birth"].str.replace('(', '', regex=True)
+            tab1["Date of birth"] = tab1["Date of birth"].str.partition(')')[0]
+            tab1["Date of birth"] = pd.to_datetime(tab1["Date of birth"])
+            dtypescols = {"Number":"int","Caps":"int","Goals":"int"}
+            tab1 = tab1.astype(dtypescols)
+            tab1.index = np.arange(1, len(tab1) + 1)
+            tab1.index.name = "playerid"
+            tab1["teamid"] = self.teamid    
+
+            return tab1
+        except Exception as inst:
+            return print(inst)
+        
     def getTeamsHead(self):
         """ Get teams """
 
@@ -44,39 +133,8 @@ class Teams:
             return res
         except:
             return print("Error occured with the link.")
-    
-    def getTeamsDetailsSoup(self):
-        try:
-            data = []            
-            page = requests.get(self.link)
-            soup = BeautifulSoup(page.text, parser='lxml')
-            table = soup.find( "table", {"class":"plainrowheaders"} )
-            if table.empty:
-                raise Exception("There is an empty dataset")
-                
-            table_body = table.find('tbody')
-            
-            rows = table_body.find_all('tr')
-            for row in rows:
-                cols = row.find_all(['td','th'])
-                cols = [ele.text.strip() for ele in cols]
-                data.append([ele for ele in cols if ele]) # Get rid of empty values    
-              
-            
-            tab1 = pd.DataFrame(data)
-            tab1 = tab1.dropna()
-            tab1.columns = tab1.iloc[0]
-            tab1 = tab1[1:]
-            dtypescols = {"No.":"int","Caps":"int","Goals":"int"}
-            tab1 = tab1.astype(dtypescols)
-            tab1.index = np.arange(1, len(tab1) + 1)
-            tab1.index.name = "playerid"
-            tab1["teamid"] = self.teamid    
 
-            return tab1
-        except Exception as inst:
-            return print(inst)
-    
+
     def getTeamsDetails(self,mtch):
         try:
             html = requests.get(self.link).content        
@@ -105,3 +163,5 @@ class Teams:
             return tab1
         except Exception as inst:
             return print(inst)
+    
+    
